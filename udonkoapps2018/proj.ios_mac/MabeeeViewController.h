6aa3037941d2d5f92ea835118db5761bc14901c3
Hidehiko Kondo <dev@udonko.net>
2018-05-16 15:55:35 +0900

    MaBeeeSDK組み込み、接続画面の呼び出しまで

---
+++ udonkoapps2018/proj.ios_mac/MabeeeViewController.h
@@ -0,0 +1,12 @@
+//
+//  MabeeeViewController.h
+//  udonkoapps2018-mobile
+//
+//  Created by Hidehiko Kondo on 2018/05/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MabeeeViewController : UIViewController
+
+@end
